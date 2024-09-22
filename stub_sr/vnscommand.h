cc499de66585fad4e1637c22cdcc32c26906e8f7
Brandon Walter Good <brandongood@cs.arizona.edu>
2024-09-22 14:15:55 -0700

    formatting

--- stub_sr/vnscommand.h
+++ stub_sr/vnscommand.h
@@ -8,11 +8,11 @@
 #ifndef __VNSCOMMAND_H
 #define __VNSCOMMAND_H
 
-#define VNSOPEN       1
-#define VNSCLOSE      2
-#define VNSPACKET     4
-#define VNSBANNER     8
-#define VNSHWINFO    16
+#define VNSOPEN 1
+#define VNSCLOSE 2
+#define VNSPACKET 4
+#define VNSBANNER 8
+#define VNSHWINFO 16
 
 #define IDSIZE 32
 
@@ -24,7 +24,7 @@ typedef struct
 {
     uint32_t mLen;
     uint32_t mType;
-}__attribute__ ((__packed__)) c_base;
+} __attribute__((__packed__)) c_base;
 
 /*-----------------------------------------------------------------------------
                                  OPEN
@@ -34,15 +34,15 @@ typedef struct
 {
 
     uint32_t mLen;
-    uint32_t mType;        /* = VNSOPEN */
-    uint16_t topoID;       /* Id of the topology we want to run on */
-    uint16_t pad;          /* unused */
-    char     mVirtualHostID[IDSIZE]; /* Id of the simulated router (e.g.
-                                        'VNS-A'); */
-    char     mUID[IDSIZE]; /* User id (e.g. "appenz"), for information only */
-    char     mPass[IDSIZE];
+    uint32_t mType;              /* = VNSOPEN */
+    uint16_t topoID;             /* Id of the topology we want to run on */
+    uint16_t pad;                /* unused */
+    char mVirtualHostID[IDSIZE]; /* Id of the simulated router (e.g.
+                                    'VNS-A'); */
+    char mUID[IDSIZE];           /* User id (e.g. "appenz"), for information only */
+    char mPass[IDSIZE];
 
-}__attribute__ ((__packed__)) c_open;
+} __attribute__((__packed__)) c_open;
 
 /*-----------------------------------------------------------------------------
                                  CLOSE
@@ -53,9 +53,9 @@ typedef struct
 
     uint32_t mLen;
     uint32_t mType;
-    char     mErrorMessage[256];
+    char mErrorMessage[256];
 
-}__attribute__ ((__packed__)) c_close;
+} __attribute__((__packed__)) c_close;
 
 /*-----------------------------------------------------------------------------
                                 HWREQUEST
@@ -67,7 +67,7 @@ typedef struct
     uint32_t mLen;
     uint32_t mType;
 
-}__attribute__ ((__packed__)) c_hwrequest;
+} __attribute__((__packed__)) c_hwrequest;
 
 /*-----------------------------------------------------------------------------
                                  BANNER
@@ -78,100 +78,99 @@ typedef struct
 
     uint32_t mLen;
     uint32_t mType;
-    char     mBannerMessage[256];
+    char mBannerMessage[256];
 
-}__attribute__ ((__packed__)) c_banner;
+} __attribute__((__packed__)) c_banner;
 
 /*-----------------------------------------------------------------------------
                                PACKET (header)
   ---------------------------------------------------------------------------*/
 
-
 typedef struct
 {
     uint32_t mLen;
     uint32_t mType;
-    char     mInterfaceName[16];
-    uint8_t  ether_dhost[6];
-    uint8_t  ether_shost[6];
+    char mInterfaceName[16];
+    uint8_t ether_dhost[6];
+    uint8_t ether_shost[6];
     uint16_t ether_type;
 
-}__attribute__ ((__packed__)) c_packet_ethernet_header;
+} __attribute__((__packed__)) c_packet_ethernet_header;
 
 typedef struct
 {
     uint32_t mLen;
     uint32_t mType;
-    char     mInterfaceName[16];
-}__attribute__ ((__packed__)) c_packet_header;
+    char mInterfaceName[16];
+} __attribute__((__packed__)) c_packet_header;
 
 /*-----------------------------------------------------------------------------
                                HWInfo
   ----------------------------------------------------------------------------*/
 
-#define HWINTERFACE    1
-#define HWSPEED        2
-#define HWSUBNET       4
-#define HWINUSE        8
-#define HWFIXEDIP     16
-#define HWETHER       32
-#define HWETHIP       64
-#define HWMASK       128
+#define HWINTERFACE 1
+#define HWSPEED 2
+#define HWSUBNET 4
+#define HWINUSE 8
+#define HWFIXEDIP 16
+#define HWETHER 32
+#define HWETHIP 64
+#define HWMASK 128
 
 typedef struct
 {
     uint32_t mKey;
-    char     value[32];
-}__attribute__ ((__packed__)) c_hw_entry;
+    char value[32];
+} __attribute__((__packed__)) c_hw_entry;
 
 typedef struct
 {
 #define MAXHWENTRIES 256
-    uint32_t   mLen;
-    uint32_t   mType;
+    uint32_t mLen;
+    uint32_t mType;
     c_hw_entry mHWInfo[MAXHWENTRIES];
-}__attribute__ ((__packed__)) c_hwinfo;
-
+} __attribute__((__packed__)) c_hwinfo;
 
 /* ******* New VNS Messages ******** */
-#define VNS_RTABLE        32
-#define VNS_OPEN_TEMPLATE 64
+#define VNS_RTABLE 32
+#define VNS_OPEN_tmplate 64
 #define VNS_AUTH_REQUEST 128
-#define VNS_AUTH_REPLY   256
-#define VNS_AUTH_STATUS  512
+#define VNS_AUTH_REPLY 256
+#define VNS_AUTH_STATUS 512
 
 /* rtable */
 typedef struct
 {
     uint32_t mLen;
     uint32_t mType;
-    char     mVirtualHostID[IDSIZE];
-    char     rtable[0];
-}__attribute__ ((__packed__)) c_rtable;
+    char mVirtualHostID[IDSIZE];
+    char rtable[0];
+} __attribute__((__packed__)) c_rtable;
 
-/* open template */
-typedef struct {
+/* open tmplate */
+typedef struct
+{
     uint32_t ip;
-    uint8_t  num_masked_bits;
-}__attribute__ ((__packed__)) c_src_filter;
+    uint8_t num_masked_bits;
+} __attribute__((__packed__)) c_src_filter;
 
 typedef struct
 {
-    uint32_t     mLen;
-    uint32_t     mType;
-    char         templateName[30];
-    char         mVirtualHostID[IDSIZE];
+    uint32_t mLen;
+    uint32_t mType;
+    char tmplateName[30];
+    char mVirtualHostID[IDSIZE];
     c_src_filter srcFilters[0];
-}__attribute__ ((__packed__)) c_open_template;
+} __attribute__((__packed__)) c_open_tmplate;
 
 /* authentication request */
 typedef struct
 {
     uint32_t mLen;
     uint32_t mType;
-    uint8_t  salt[0];
+    uint8_t salt[0];
 
-}__attribute__ ((__packed__)) c_auth_request;
+} __attribute__((__packed__)) c_auth_request;
 
 /* authentication reply */
 typedef struct
@@ -179,18 +178,18 @@ typedef struct
     uint32_t mLen;
     uint32_t mType;
     uint32_t usernameLen;
-    char     username[0];
+    char username[0];
     /* remainder of the message is the salted sha1 of the user's password */
-}__attribute__ ((__packed__)) c_auth_reply;
+} __attribute__((__packed__)) c_auth_reply;
 
 /* authentication status (whether or not a reply was accepted) */
 typedef struct
 {
     uint32_t mLen;
     uint32_t mType;
-    uint8_t  auth_ok;
-    char     msg[0];
+    uint8_t auth_ok;
+    char msg[0];
 
-}__attribute__ ((__packed__)) c_auth_status;
+} __attribute__((__packed__)) c_auth_status;
 
-#endif  /* __VNSCOMMAND_H */
+#endif /* __VNSCOMMAND_H */
